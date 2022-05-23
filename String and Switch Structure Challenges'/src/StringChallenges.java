eb3ebf79db37e919926971f2f6e7158a6e4a3531
nicholas.dohr <nicholas.dohr@MU-C1-09.mullen.pvt.k12.co.us>
2022-05-23 13:33:59 -0600

    Switch Structure Challenges

---
+++ String and Switch Structure Challenges'/src/StringChallenges.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class StringChallenges
+	{
+
+		public static void main(String[] args)
+			{
+				Scanner userStringInput = new Scanner(System.in);
+				System.out.println("Hello user, what is your name?");
+				String name = userStringInput.nextLine();
+				switch(name)
+				{
+					case N:
+					case I:
+					case C:
+					case K:
+							{
+								System.out.println("We share letters of our name!");
+								break;
+							}
+					
+				}
+
+			}
+
+	}
